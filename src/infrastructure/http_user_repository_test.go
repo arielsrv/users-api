904278a47bb3a509247c802073308e0649988952
Ariel Piñeiro <ariel.pineiro@iskaypet.com>
2022-07-16 10:50:50 +0200

    coverage

--- src/infrastructure/http_user_repository_test.go
+++ src/infrastructure/http_user_repository_test.go
@@ -3,6 +3,8 @@ package infrastructure
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
+	"github.com/gofiber/fiber/v2"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/suite"
 	"github.com/users-api/src/domain"
@@ -15,10 +17,12 @@ type HttpUserRepositoryUnitSuite struct {
 	suite.Suite
 	client         *MockClient
 	userRepository *HttpUserRepository
+	errorClient    *MockErrorClient
 }
 
 func (suite *HttpUserRepositoryUnitSuite) SetupTest() {
 	suite.client = new(MockClient)
+	suite.errorClient = new(MockErrorClient)
 	suite.userRepository = NewHttpUserRepository(suite.client)
 }
 
@@ -36,6 +40,16 @@ func (mock *MockClient) Get(string) (response *http.Response, err error) {
 	return result.(*http.Response), nil
 }
 
+type MockErrorClient struct {
+	mock.Mock
+}
+
+func (mock *MockErrorClient) Get(string) (response *http.Response, err error) {
+	args := mock.Called()
+	result := args.Get(0)
+	return result.(*http.Response), errors.New("A error has ocurred. ")
+}
+
 func (suite *HttpUserRepositoryUnitSuite) TestGet() {
 	suite.client.On("Get").Return(Get())
 
@@ -46,6 +60,34 @@ func (suite *HttpUserRepositoryUnitSuite) TestGet() {
 	suite.Equal(1, actual.Id)
 	suite.Equal("John Doe", actual.Name)
 	suite.Equal("john@doe.com", actual.Email)
+}
+
+func (suite *HttpUserRepositoryUnitSuite) TestGet_NotFound() {
+	suite.client.On("Get").Return(GetNotFound())
+
+	actual, err := suite.userRepository.GetUser(1)
+
+	suite.Nil(actual)
+	suite.Error(err)
+	if e, ok := err.(*fiber.Error); ok {
+		suite.Equal(http.StatusNotFound, e.Code)
+		suite.Equal("Couldn't retreive user with id 1 not found. ", e.Message)
+	}
+}
+
+func (suite *HttpUserRepositoryUnitSuite) TestGet_InternalServerError() {
+	suite.errorClient.On("Get").Return(&http.Response{})
+
+	userRepository := NewHttpUserRepository(suite.errorClient)
+
+	actual, err := userRepository.GetUser(1)
+
+	suite.Nil(actual)
+	suite.Error(err)
+	if e, ok := err.(*fiber.Error); ok {
+		suite.Equal(http.StatusInternalServerError, e.Code)
+		suite.Equal("A error has ocurred. ", e.Message)
+	}
 }
 
 func (suite *HttpUserRepositoryUnitSuite) TestGetAll() {
@@ -76,6 +118,12 @@ func Get() (response *http.Response, err error) {
 	}
 	binary, err := json.Marshal(user)
 	return buildResponse(binary)
+}
+
+func GetNotFound() (response *http.Response, err error) {
+	return &http.Response{
+		StatusCode: http.StatusNotFound,
+	}, nil
 }
 
 func GetAll() (response *http.Response, err error) {
