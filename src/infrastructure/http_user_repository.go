3240cc99ffa4cee83c9f0461ccddeaec903dfd33
Ariel Piñeiro <ariel.pineiro@iskaypet.com>
2022-07-19 11:17:41 +0200

    fix

--- src/infrastructure/http_user_repository.go
+++ src/infrastructure/http_user_repository.go
@@ -1,28 +1,30 @@
 package infrastructure
 
 import (
+	"fmt"
 	"github.com/users-api/src/domain"
-	"strconv"
 )
 
 type HttpUserRepository struct {
-	client HttpClient
+	client  HttpClient
+	baseUrl string
 }
 
 func NewHttpUserRepository(client HttpClient) *HttpUserRepository {
 	return &HttpUserRepository{
-		client: client,
+		client:  client,
+		baseUrl: "https://gorest.co.in/public/v2",
 	}
 }
 
 func (repository HttpUserRepository) GetUser(userId int) (*domain.User, error) {
-	url := "https://gorest.co.in/public/v2/users/" + strconv.Itoa(userId)
+	url := fmt.Sprintf("%s/users/%d", repository.baseUrl, userId)
 	user, err := Client[domain.User]{client: repository.client}.Get(url)
 	return &user, err
 }
 
 func (repository HttpUserRepository) GetUsers() ([]domain.User, error) {
-	url := "https://gorest.co.in/public/v2/users/"
+	url := fmt.Sprintf("%s/users", repository.baseUrl)
 	users, err := Client[[]domain.User]{client: repository.client}.Get(url)
 	return users, err
 }
