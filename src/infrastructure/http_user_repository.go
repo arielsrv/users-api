510c62c29111608bb264cdfcd49c07349a110e63
ariel.pineiro@iskaypet.com <arielsrv@gmail.com>
2022-07-19 13:34:36 +0200

    pretty

--- src/infrastructure/http_user_repository.go
+++ src/infrastructure/http_user_repository.go
@@ -19,12 +19,12 @@ func NewHttpUserRepository(client HttpClient) *HttpUserRepository {
 
 func (repository HttpUserRepository) GetUser(userId int) (*domain.User, error) {
 	url := fmt.Sprintf("%s/users/%d", repository.baseUrl, userId)
-	user, err := Client[domain.User]{client: repository.client}.Get(url)
+	user, err := Client[domain.User]{repository.client}.Get(url)
 	return &user, err
 }
 
 func (repository HttpUserRepository) GetUsers() ([]domain.User, error) {
 	url := fmt.Sprintf("%s/users", repository.baseUrl)
-	users, err := Client[[]domain.User]{client: repository.client}.Get(url)
+	users, err := Client[[]domain.User]{repository.client}.Get(url)
 	return users, err
 }
